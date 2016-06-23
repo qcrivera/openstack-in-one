51e4e0467414231b55ad9afa3e12033dac0d174d
qcrivera <crivera@bell.net>
2016-06-23 13:51:06 -0400

    Update iptables.sh

--- files/iptables.sh
+++ files/iptables.sh
@@ -1,2 +1,2 @@
 iptables -I FORWARD -d 172.30.200.4/32 -p tcp -m state --state NEW -m tcp --dport 80 -j ACCEPT
-iptables -t nat -A PREROUTING -d 169.54.100.74/32 -p tcp --dport 80 -j DNAT --to 172.30.200.4:80
+iptables -t nat -A PREROUTING -d $(facter ipaddress_bond1)/32 -p tcp --dport 80 -j DNAT --to 172.30.200.4:80
